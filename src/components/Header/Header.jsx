67d6c0989e445473623b9baff2249a13aaa7a279
bekirbali <bfbali43@gmail.com>
2023-02-18 19:58:29 +0300

    nba legends

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,11 @@
+import logo from "../../utils/nba-logo.png";
+import styles from "./Header.module.scss";
+import { FaTimes } from "react-icons/fa";
+
 const Header = () => {
   return (
-    <div>
+    <div className={styles.header}>
+      <img src={logo} alt="" />
       <h2>NBA LEGENDS</h2>
     </div>
   );
